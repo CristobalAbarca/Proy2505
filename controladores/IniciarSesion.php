21c1bc2309eacf7622df28dfd7ecf0e0a534538a
cetecom <cetecom@LC08L1228>
2017-05-25 13:19:08 -0400

    Crear usuario y cambiar su clave

--- controladores/IniciarSesion.php
+++ controladores/IniciarSesion.php
@@ -1,14 +1,18 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Description of LogTransaccion
- *
- * @author cetecom
- */
-class LogTransaccion {
-    //put your code here
+session_start();
+include '../lib/Conexion.php';
+include '../dato/Usuario.php';
+$oUsr = new Usuario();
+$oUsr->nombre=$_POST['nombre'];
+$oUsr->clave=$_POST['pass'];
+if($oUsr->VerificarUsuarioContrasenna())
+{
+    $_SESSION["Usuario"]=$oUsr;
+    header('location:http://localhost:8081/Proy2305/index.php');
 }
+else
+{
+    echo 'No existe';
+    
+}
+
--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+include("constantes.php");
+include("librerias.php");
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <a href="formularios/formcambiarclave.php">Cambiar clave de usuario</a>
+        <a href="formularios/formagregarproducto.php"> Agregar Producto</a>
         <?php
-        // put your code here
+        echo PATH;
         ?>
     </body>
 </html>
